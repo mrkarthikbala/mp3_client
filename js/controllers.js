b6dc7706b8052547bf3f95c6ed91c047ec38d40d
Karthik Bala <kbala2@illinois.edu>
2015-04-09 18:25:13 -0500

    everything is functional

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,7 @@
 var taskManagerControllers = angular.module('taskManagerControllers', []);
  
 
-taskManagerControllers.controller('UserController', ['$scope','$http', 'Users', function($scope, $http, Users){
+taskManagerControllers.controller('UserController', ['$scope','$http', 'Users', 'Tasks', function($scope, $http, Users, Tasks){
 
   $scope.newUserName = "Username";
   $scope.newUserEmail = "Email";
@@ -13,8 +13,18 @@ taskManagerControllers.controller('UserController', ['$scope','$http', 'Users', 
     });
   };
 
-  $scope.deleteUser = function(id){
-    Users.deleteUser(id).success(function(data){
+  $scope.deleteUser = function(id,name){
+  
+
+    Tasks.getTasks("?where={\"assignedUser\": \"" + id + "\", \"assignedUserName\": \"" + name + "\"}").success(function(data){
+        for (var i = 0; i < data.data.length; i++){
+          data.data[i].assignedUser = "";
+          data.data[i].assignedUserName = "unassigned";
+          Tasks.updateTask(data.data[i]);
+        }
+    });
+
+      Users.deleteUser(id).success(function(data){
       $scope.getUsers();
     });
   };
@@ -43,18 +53,8 @@ taskManagerControllers.controller('UserDetailController', ['$scope', '$routePara
     Users.getUser($routeParams.id).success(function(data){
       $scope.user = data.data;
 
-      //SHOULD BE QUERYING TASKS WHERE username = my name
-
-      var hardQuotes = "";
-      for (var i = 0; i < $scope.user.pendingTasks.length; i++){
-        hardQuotes += "\"" + $scope.user.pendingTasks[i] + "\",";
-      }
-      //edge case no pending tasks
-      var query = '?where={\"_id\": {\"$in\": ['+hardQuotes.substring(0, hardQuotes.length-1)+
-      '] }}';
-      
-     Tasks.getTasks(query).success(function(data){
-         $scope.pendingTasks = data.data;
+    Tasks.getTasks("?where={\"assignedUser\": \"" + $scope.user._id + "\", \"completed\": false}").success(function(data){
+      $scope.pendingTasks = data.data;
     });
     });
   
@@ -73,16 +73,14 @@ taskManagerControllers.controller('UserDetailController', ['$scope', '$routePara
         var user = $scope.user;
         for (var i = 0; i < user.pendingTasks.length; i++){
           if (user.pendingTasks[i] == id){
-           user.pendingTasks.splice(i, 1);
-           alert("hi");}
+           user.pendingTasks.splice(i, 1);}
         }
         Users.updateUser(user);
-
+        $scope.loadCompletedTasks($scope.user.name);
     };
 
     $scope.loadCompletedTasks = function(name){
-      //SELECT * FrOM TASKS WHERE assignedUserName = name
-      Tasks.getTasks("?where={\"assignedUserName\":" +"\""+ name + "\""+"}").success(function(data){
+      Tasks.getTasks("?where={\"assignedUserName\":" +"\""+ name + "\""+", \"completed\": true}").success(function(data){
         $scope.completedTasks = data.data;
       });
 
@@ -91,6 +89,19 @@ taskManagerControllers.controller('UserDetailController', ['$scope', '$routePara
 }]);
 
 taskManagerControllers.controller('TaskDetailController', ['$scope', '$routeParams', '$http', 'Users','Tasks', function($scope, $routeParams, $http, Users,Tasks){
+  //get possible assigned users
+   Users.getUsers("?select={\"name\": 1, \"_id\": 1}").success(function(data){
+      $scope.possibleAssignedUsers = data.data;
+      $scope.possibleAssignedUsers.unshift({name: "Unassigned"});
+    });
+  $scope.submit = function(){
+    if ($scope.assigned){
+      $scope.task.assignedUser = $scope.assigned._id;
+      $scope.task.assignedUserName = $scope.assigned.name;
+    }
+    Tasks.updateTask($scope.task);
+
+  }
   var completionButton = function(){
     if ($scope.task.completed == true){
       $scope.Complete = "Mark Uncompleted";
@@ -110,45 +121,100 @@ taskManagerControllers.controller('TaskDetailController', ['$scope', '$routePara
     Tasks.updateTask($scope.task);
   };
 }]);
-taskManagerControllers.controller('TaskController', ['$scope', '$http', 'Tasks', function($scope, $http, Tasks){
+taskManagerControllers.controller('TaskController', ['$scope', '$http','Users', 'Tasks', function($scope, $http, Users, Tasks){
   $scope.skip=0;
   $scope.newTaskName = "Name";
   $scope.completed="pending";
+  $scope.sortBy = "sortByName";
+  $scope.ascendingOrder = "true";
 
-    $scope.$watch('completed', function() {
+   
+    $scope.$watchGroup(['ascendingOrder','sortBy', 'completed'], function() {
        $scope.getTasks();
    });
-  var whichTasks = function(){  //returns query based on value of $scope.completed
+  var whatToSortBy = function(){
+    var asc;
+    if ($scope.ascendingOrder === "true") asc = 1;
+    else asc = -1;
 
+    if ($scope.sortBy ==="sortByName"){
+      return "&sort={\"name\": "+ asc+"}";
+    }
+    else if ($scope.sortBy === "sortByUserName"){
+      return "&sort={\"assignedUserName\": " + asc + "}";
+    }
+    else if ($scope.sortBy ==="sortByDateCreated"){
+      return "&sort={\"dateCreated\":"+ asc+"}";
+    }
+    else return "&sort={\"deadline\": "+asc+"}";
+  };
+
+  var whichTasks = function(){  //returns query based on value of $scope.completed
+    var order = whatToSortBy();
     if ($scope.completed ==="pending"){
-      return "?where={\"completed\": false}&select={\"name\": 1, \"assignedUserName\": 1 }&skip="+ $scope.skip + "&limit=10";
+      if ($scope.was == "completed" || $scope.was == "all") $scope.skip = 0;
+      return "?where={\"completed\": false}&select={\"name\": 1, \"assignedUserName\": 1 }"+ order + "&skip="+ $scope.skip + "&limit=10";
     } 
     if ($scope.completed ==="completed"){
-      return "?where={\"completed\": true}&select={\"name\": 1, \"assignedUserName\": 1 }&skip="+ $scope.skip + "&limit=10";
+      if ($scope.was == "pending" || $scope.was == "all") $scope.skip = 0;
+      return "?where={\"completed\": true}&select={\"name\": 1, \"assignedUserName\": 1 }" + order + "&skip="+ $scope.skip + "&limit=10";
     }
-    return "?select={\"name\": 1, \"assignedUserName\": 1 }&skip="+ $scope.skip + "&limit=10";
+    if ($scope.was == "pending" || $scope.was == "completed") $scope.skip = 0;
+    return "?select={\"name\": 1, \"assignedUserName\": 1 }" + order + "&skip="+ $scope.skip + "&limit=10";
   };
+
+//ADD NEW TASK
+    //load users to assign task to
+    Users.getUsers("?select={\"name\": 1, \"_id\": 1}").success(function(data){
+      $scope.possibleAssignedUsers = data.data;
+      $scope.possibleAssignedUsers.unshift({name: "Unassigned"});
+    });
 
   $scope.addTask = function(){
     if ($scope.newTaskName == "Name"){
       alert("Please enter a valid task name");
       return;
     }
-    Tasks.postTask({"name" : $scope.newTaskName, "description": $scope.newTaskDescription, "deadline" : $scope.newTaskDeadline}).success(function(data){
-      alert("Posted.");
+    //case assigned user is unassigned
+    if ($scope.addAssignedUser == undefined) alert("Please add an assigned user for this task.");
+    if ($scope.addAssignedUser.name == "Unassigned"){
+      Tasks.postTask({"name" : $scope.newTaskName, "description": $scope.newTaskDescription, 
+      "deadline" : $scope.newTaskDeadline, "completed" : false, "assignedUser" : "", "assignedUserName": "unassigned"
+    }).success(function(data){
+            alert("Task Added!");
+    }).error(function(err){
+      alert(err.message);
     });
+    return;
   }
+  //otherwise
+    Tasks.postTask({"name" : $scope.newTaskName, "description": $scope.newTaskDescription, 
+      "deadline" : $scope.newTaskDeadline, "completed" : false, "assignedUser" : $scope.addAssignedUser._id, "assignedUserName": $scope.addAssignedUser.name
+    }).success(function(data){
+        alert("Task Added!");
+
+    }).error(function(err){
+      alert(err.message);
+    });
+
+  }
+
   $scope.getNext = function(num){
+    if ($scope.tasks.length < 10 && num > 0) return;
     $scope.skip += num;
     if ($scope.skip < 0) $scope.skip = 0;
+    $scope.was = $scope.completed;
     $scope.getTasks();
   }
-  //where={completed:"+ false+ "}&
   $scope.getTasks = function(id){
     var query = whichTasks();
     Tasks.getTasks(query).success(function(data){
       $scope.tasks = data.data;
     });
+  };
+  $scope.countTasks = function(){
+    if ($scope.tasks.length < 10) return true;
+    return false;
   };
   $scope.deleteTask = function(id){
     Tasks.deleteTask(id).success(function(id){
